80184e6d9f5ad851bb47fcba8d1094f25da3fda6
EvilTomato9890 <omglol9898719@gmail.com>
2025-12-19 18:02:15 +0300

    Garbage clean

--- project_headers.h
+++
@@ -1,8 +0,0 @@
-// Auto-generated file with all project headers
-// DO NOT EDIT MANUALLY
-
-#include "project/backend/include/backend.h"
-#include "project/frontend/error_logger/include/frontend_err_logger.h"
-#include "project/midend/include/tree_optimize.h"
-#include "project/frontend/lexer/include/lexer_tokenizer.h"
-#include "project/frontend/parser/include/frontend_parser.h"
